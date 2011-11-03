d2c258b6d622dbc5a199735d019514e25d36450c
Gary Pennington <garypen@gmail.com>
2011-11-03 07:07:54 +0000

    Change tools.cli dependency to 0.2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject secret-santa "0.1.0-SNAPSHOT"
   :description "choose and email secret santa allocations to participants"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/tools.cli "0.2.0-SNAPSHOT"]
+                 [org.clojure/tools.cli "0.2.0"]
                  [org.apache.commons/commons-email "1.2"]]
   :main secret-santa.core)
